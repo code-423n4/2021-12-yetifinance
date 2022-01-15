08ac506bbe66f7aef62efe98501878d2671b2ef4
truco <truco@yetifinance.co>
2022-01-14 23:32:08 -0800

    Fix some compile time errors

--- packages/contracts/contracts/YETI/IsYETIRouter.sol
+++ packages/contracts/contracts/YETI/IsYETIRouter.sol
@@ -5,5 +5,5 @@ pragma solidity 0.6.12;
 
 interface IsYETIRouter {
     // Must require that the swap went through successfully with at least min yeti out amounts out. 
-    function swap(uint256 _YUSDAmount, uint256 _minYETIOut, address _to) external returns (uint256[] amounts);
+    function swap(uint256 _YUSDAmount, uint256 _minYETIOut, address _to) external returns (uint256[] memory amounts);
 }
--- packages/contracts/contracts/YETI/YUSDToYETIRouters/dummyUniV2Router.sol
+++ packages/contracts/contracts/YETI/YUSDToYETIRouters/dummyUniV2Router.sol
@@ -1,8 +1,8 @@
 pragma solidity 0.6.12;
 
 import "../IsYETIRouter.sol";
-import "./BoringCrypto/BoringOwnable.sol";
-import "./BoringCrypto/IERC20.sol";
+import "../BoringCrypto/BoringOwnable.sol";
+import "../BoringCrypto/IERC20.sol";
 
 // Dummy contract for swapping just in one swap from YUSD to YETI 
 // in one univ2 pool with the path being just that. Testing purposes only.
@@ -26,11 +26,11 @@ contract dummyUniV2Router is IsYETIRouter, BoringOwnable {
         transferOwnership(address(0), true, true);
     }
 
-    function swap(uint256 _YUSDAmount, uint256 _minYETIOut, address _to) external override returns (uint256[] amounts) {
+    function swap(uint256 _YUSDAmount, uint256 _minYETIOut, address _to) external override returns (uint256[] memory amounts) {
         address cachedJOERouterAddress = JOERouterAddress;
         IERC20 cachedYUSDToken = yusdToken;
         require(cachedYUSDToken.approve(cachedJOERouterAddress, 0));
-        require(cachedYUSDToken.increaseAllowance(cachedJOERouterAddress, _YUSDToSell));
+        require(cachedYUSDToken.increaseAllowance(cachedJOERouterAddress, _YUSDAmount));
         amounts = JOERouter.swapExactTokensForTokens(_YUSDAmount, _minYETIOut, path, _to, block.timestamp);
     }
 }
